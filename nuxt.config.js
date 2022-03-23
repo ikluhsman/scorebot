1a2ef78dd02287406ab8a0fb4949010664b9eef1
Ian Kluhsman <iankluhsman@gmail.com>
2022-03-23 11:04:15 -0600

    Added plugin for reusable validation methods

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,7 +25,8 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    { src: '~/plugins/directives', ssr: false }
+    { src: '~/plugins/directives', ssr: false },
+    { src: '~/plugins/validate', ssr: false }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
