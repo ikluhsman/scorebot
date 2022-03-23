68b10459c66170eeac476bf2657a9e1ef8de455a
Ian Kluhsman <iankluhsman@gmail.com>
2022-03-23 01:15:43 -0600

    Added ability to change game

--- components/ScoreBoard.vue
+++ components/ScoreBoard.vue
@@ -1,10 +1,25 @@
 <template>
   <div id="score-board" class="bg-gray-900 text-lime-300 w-full p-4 z-10">
     <div class="flex justify-between items-start pb-2">
-      <div>{{ datetime }}<br>{{ game }}</div>
+      <div>
+        <span>{{ datetime }}</span>
+        <div @click="editGame = true">
+          <span>Game:</span>
+          <input
+            v-if="editGame"
+            id="game"
+            v-focus
+            :value="game"
+            class="bg-transparent"
+            autocomplete="off"
+            @change="changeGame"
+          >
+          <span v-else>{{ game }}</span>
+        </div>
+      </div>
       <div>
         <div class="flex gap-4">
-          <span>Goal </span><input id="goal" class="w-8 bg-transparent" autocomplete="off" :value="goal" @change="changeGoal"></input>
+          <span>Goal: </span><input id="goal" class="w-16 bg-transparent" autocomplete="off" :value="goal" @change="changeGoal"></input>
         </div>
         <div>
           Current Leader: {{ leaders }}
@@ -39,12 +54,16 @@ export default {
   },
   data () {
     return {
-      leader: null
+      leader: null,
+      editGame: false
     }
   },
   methods: {
     changeGoal () {
       this.$emit('changeGoal', document.getElementById('goal').value)
+    },
+    changeGame () {
+      this.$emit('changeGame', document.getElementById('game').value)
     }
   }
 }
