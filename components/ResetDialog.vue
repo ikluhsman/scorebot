55a077d849554edab07e39c3ac8e1b5d4840a2b7
Ian Kluhsman <iankluhsman@gmail.com>
2022-03-23 11:08:36 -0600

    Simplify event emitting elements

--- components/ResetDialog.vue
+++ components/ResetDialog.vue
@@ -16,7 +16,7 @@
               <path fill="currentColor" d="M9,20.42L2.79,14.21L5.62,11.38L9,14.77L18.88,4.88L21.71,7.71L9,20.42Z" />
             </svg>
           </button>
-          <button class="text-red-500" @click="no">
+          <button class="text-red-500" @click="close">
             <svg style="width:24px;height:24px" viewBox="0 0 24 24">
               <path fill="currentColor" d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" />
             </svg>
@@ -28,22 +28,12 @@
 </template>
 <script>
 export default {
-  data () {
-    return {
-      response: false
-    }
-  },
   methods: {
     close () {
-      this.$emit('closeModal', this.response)
+      this.$emit('closeModal', false)
     },
     yes () {
-      this.response = true
-      this.$emit('closeModal', this.response)
-    },
-    no () {
-      this.response = false
-      this.$emit('closeModal', this.response)
+      this.$emit('closeModal', true)
     }
   }
 }
