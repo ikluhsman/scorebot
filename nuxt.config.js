ba5b61d2cce2934cf3b2bbc48a3989377806ae6c
Ian Kluhsman <iankluhsman@gmail.com>
2022-03-25 15:31:44 -0600

    Updated SEO tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,10 +13,40 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'scorebot.app', name: 'ScoreBot', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
+      {
+        hid: 'og-title',
+        property: 'og:title',
+        content: 'ScoreBot'
+      },
+      {
+        hid: 'og-description',
+        property: 'og:description',
+        content: 'Scorebot is an app for keeping score of many different games.'
+      },
+      {
+        hid: 'og-type',
+        property: 'og:type',
+        content: 'website'
+      },
+      {
+        hid: 'og-image',
+        property: 'og:image',
+        content: 'https://scorebot.app/splash.jpg'
+      },
+      {
+        hid: 'og-url',
+        property: 'og:url',
+        content: 'https://scorebot.app/'
+      }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/pound.svg' }
+      { rel: 'icon', type: 'image/x-icon', href: '/pound.svg' },
+      {
+        hid: 'canonical',
+        rel: 'canonical',
+        href: 'https://scorebot.app/'
+      }
     ]
   },
   css: [
