4eb6a0ffdcc34aa859516b50202b62e9342f2e1e
Ian Kluhsman <iankluhsman@gmail.com>
2022-03-25 06:26:34 -0600

    Started adding SEO tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,14 +8,19 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'scorebot',
+    htmlAttrs: {
+      lang: 'en'
+    },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'scorebot.app', name: 'ScoreBot', content: '' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://use.typekit.net/vom4nrc.css' }]
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/pound.svg' },
+      { rel: 'stylesheet', href: 'https://use.typekit.net/vom4nrc.css' }
+    ]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
--- pages/scores/_id.vue
+++ pages/scores/_id.vue
@@ -10,8 +10,47 @@ export default {
       jsonData = JSON.parse(b)
     }
     return {
-      jsonData
+      jsonData, 
+      id: query.id
     }
-  }
+  },
+  head () {
+    return {
+      title: 'ScoreBot',
+      link: [
+        {
+          hid: 'canonical',
+          rel: 'canonical',
+          href: 'https://scorebot.app/' + this.id
+        }
+      ],
+      meta: [
+        {
+          hid: 'og:title',
+          property: 'og:title',
+          content: 'ScoreBot'
+        },
+        {
+          hid: 'og:description',
+          property: 'og:description',
+          content: 'Scorebot is an app for keeping score of card games, darts, dominoes, dice, or any other game where you keep a basic running total of points. It is easy to use on a tablet or cell phone at the game table.'
+        },
+        {
+          hid: 'og:type',
+          property: 'og:type',
+          content: 'website'
+        },
+        {
+          hid: 'og:image',
+          property: 'og:image',
+          content: 'https://scorebot.app/pound.svg'
+        },
+        {
+          hid: 'og:url',
+          property: 'og:url',
+          content: 'https://scorebot.app'
+        }
+      ]
+    }
 }
 </script>
