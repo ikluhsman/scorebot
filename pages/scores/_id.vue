a64a2f52f3183763617545a48a7c2f56aa6c386a
Ian Kluhsman <iankluhsman@gmail.com>
2022-03-26 13:34:24 -0600

    Testing SEO on posts with ids

--- pages/scores/_id.vue
+++ pages/scores/_id.vue
@@ -1,5 +1,8 @@
 <template>
-  <score-bot ref="scoreBot" :key="'scoreBot'" :json-data="jsonData" />
+  <div>
+    <p>{{ players }}</p>
+    <score-bot ref="scoreBot" :key="'scoreBot'" :json-data="jsonData" />
+  </div>
 </template>
 <script>
 export default {
@@ -13,6 +16,20 @@ export default {
       jsonData,
       id: query.id
     }
+  },
+  head: {
+    meta: [
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content: 'https://scorebot.app/og-image-mobile.jpg'
+      },
+      {
+        hid: 'og:description',
+        property: 'og:description',
+        content: 'Link to your saved ScoreBot game'
+      }
+    ]
   }
 }
 </script>
