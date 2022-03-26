d7a3b1098d5d68bc95a780f981b2baf7f249e79b
Ian Kluhsman <iankluhsman@gmail.com>
2022-03-26 12:44:53 -0600

    Updated SEO images

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,7 +37,7 @@ export default {
       {
         hid: 'og:image',
         property: 'og:image',
-        content: 'https://scorebot.app/scorebot.jpg?1234'
+        content: 'https://scorebot.app/ogimage.jpg'
       },
       {
         hid: 'og:url',
@@ -52,7 +52,7 @@ export default {
       {
         hid: 'twitter:image',
         name: 'twitter:image',
-        content: 'https://scorebot.app/scorebot.jpg'
+        content: 'https://scorebot.app/ogimage.jpg'
       },
       {
         hid: 'twitter:description',
